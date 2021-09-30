b9e77a5f73b2eddcde4c77159a1580af39c13cc4
John Paul Alimango <esze0909@gmail.com>
2021-09-30 22:20:11 +0800

    final finally

--- src/components/Main/Game/PlayAgainBtn/PlayAgainBtn.style.js
+++ src/components/Main/Game/PlayAgainBtn/PlayAgainBtn.style.js
@@ -35,8 +35,6 @@ export const StyledPlayAgainBtn = styled.button`
   width: auto;
   &::before {
     content: "${(props) => `${props.result}`}";
-
-    @import url("https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@600;700&display=swap");
     font-family: "Barlow Semi Condensed", sans-serif;
     position: absolute;
     font-weight: 700;
